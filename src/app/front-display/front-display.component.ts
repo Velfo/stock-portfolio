5ab1752a008b98ed7ffcd6f19e7a4238916ada7d
VUGAR <aghayev.vugar@gmail.com>
2018-05-16 21:25:22 +1000

    refactoring
    adding function comments and refactoring

--- src/app/front-display/front-display.component.ts
+++ src/app/front-display/front-display.component.ts
@@ -8,18 +8,20 @@ import 'rxjs/add/operator/map';
 })
 export class FrontDisplayComponent implements OnInit {
   constructor(private http: HttpClient) { }
-
   ngOnInit() {
     this.setCurrentStockPrices();
   }
-
+  /**
+   * Makeing API calls to the Quandl to get the stocks data
+   * Getting data for Boeing and Pfizer stocks
+   * Uses https://www.quandl.com/data/EOD-End-of-Day-US-Stock-Prices
+   * Storing the data in the localStorage */
   setCurrentStockPrices() {
     //set the date for today
     let today = new Date();
     let dd = today.getDate() - 1; // to get yesterdays price, becasue the latest price available is a day before
     let mm = today.getMonth() + 1; // January is 0!
     let yyyy = today.getFullYear();
-
     let dateString = yyyy + '-' + mm + '-' + dd;
     let apiTocken = 'Wzssee7oejgiqC3HBrCF';
     let quandlUrlBoeing = 'https://www.quandl.com/api/v3/datasets/EOD/BA.json?start_date=' + dateString + '&api_key=' + apiTocken;
@@ -33,15 +35,10 @@ export class FrontDisplayComponent implements OnInit {
             .set('Content-Type', 'application/json').set('Access-Control-Allow-Origin', '*')
         }
       ).subscribe(data => {
-
       const apiData: any = data;
-
       localStorage.setItem('boeingPrice', JSON.stringify(apiData.dataset.data[0][4] ));
       localStorage.setItem('boeingName', JSON.stringify(apiData.dataset.name ));
-
-      console.log(apiData.dataset);
     });
-
     // Pfizer Inc. (PFE) Stock Price
     this
       .http
@@ -51,27 +48,9 @@ export class FrontDisplayComponent implements OnInit {
             .set('Content-Type', 'application/json').set('Access-Control-Allow-Origin', '*')
         }
       ).subscribe(data => {
-
       const apiData: any = data;
-
       localStorage.setItem('pfizerPrice', JSON.stringify(apiData.dataset.data[0][4]) );
       localStorage.setItem('pfizerName', JSON.stringify(apiData.dataset.name ));
-
-      console.log(apiData.dataset);
     });
-
   }
-
-  getBoeingApi() {
-    console.log(JSON.parse( localStorage.getItem('boeingPrice')  ) );
-    console.log(JSON.parse( localStorage.getItem('boeingName')  ) );
-    console.log(JSON.parse( localStorage.getItem('shares')  ) );
-    const testArray: any = JSON.parse( localStorage.getItem('shares')  );
-    console.log('The array is ', testArray[0]);
-  }
-  getPfizerApi() {
-    console.log(  JSON.parse(   localStorage.getItem('pfizerPrice')  ) );
-    console.log(JSON.parse( localStorage.getItem('pfizerName')[1]  ) );
-  }
-
 }
--- src/app/stocks-in-market/stocks-in-market.component.ts
+++ src/app/stocks-in-market/stocks-in-market.component.ts
@@ -37,6 +37,9 @@ export class StocksInMarketComponent implements OnInit {
   ngOnInit() {
     this.initialiseMoneyBalance().then(() => this.money = this.moneyBalanceService.getMoneyBalance());
   }
+  /**
+   * Displays the dashboard with the stocks in the market, stock holding by the user and the money balance.
+   * Uses the localStorage to initialise the variables */
   displayStocks() {
     this.stockName1 = JSON.parse(   localStorage.getItem('pfizerName')  );
     this.stockName2 = JSON.parse( localStorage.getItem('boeingName')  );
@@ -53,6 +56,8 @@ export class StocksInMarketComponent implements OnInit {
       this.money = this.moneyBalanceService.getMoneyBalance();
     }
   }
+  /**
+   * Placing buy orders on the two stocks available in the market */
   placeBuyOrder(stockName: string, orderPrice: number, orderQuantity: number) {
     let moneyToSpend = orderPrice * orderQuantity;
     let moneyInBalance =  this.moneyBalanceService.getMoneyBalance();
@@ -75,6 +80,8 @@ export class StocksInMarketComponent implements OnInit {
         break;
     }
   }
+  /**
+   * Placing sell orders on the two stocks that the user can have in the portfolio */
   placeSellOrder(stockName: string, orderPrice: number, orderQuantity: number) {
     let moneyToReceive = orderPrice * orderQuantity;
     let moneyInBalance =  this.moneyBalanceService.getMoneyBalance();
@@ -97,6 +104,8 @@ export class StocksInMarketComponent implements OnInit {
         break;
     }
   }
+  /**
+   * Inrementing the shares quantity when new shares are bought */
   pushToSharesArray(stockName: string, orderQuantity: number){
     if ( this.sharesBalance.length > 0 ) {
       for (let i of this.sharesBalance) {
@@ -115,7 +124,8 @@ export class StocksInMarketComponent implements OnInit {
       }
     }
   }
-
+  /**
+   * Decrementing the shares from the array when tha shares are sold */
   removeFromSharesArray(stockName: string, orderQuantity: number){
     if ( this.sharesBalance.length > 0 ) {
       for (let i of this.sharesBalance) {
@@ -134,7 +144,8 @@ export class StocksInMarketComponent implements OnInit {
       }
     }
   }
-
+  /**
+   * Notification to display when the order (sell or buy) are placed */
   displayOrderPlacedNotification() {
     this.orderHasBeenPlaced = true;
     setTimeout(() => {
@@ -142,24 +153,33 @@ export class StocksInMarketComponent implements OnInit {
       this.money = this.moneyBalanceService.getMoneyBalance();
     }, 1000);
   }
+  /**
+   * Notification to display when there is not enough shares to be sold */
   displayNotEnoughtSharesNotification() {
     this.shareBalanceTooLow = true;
     setTimeout(() => {
       this.shareBalanceTooLow = false;
     }, 1000);
   }
+  /**
+   * Notification to display when the price set to buy a share is too low */
   displayPriceTooLowNotification() {
     this.priceTooLow = true;
     setTimeout(() => {
       this.priceTooLow = false;
     }, 1000);
   }
+  /**
+   * Notification to display when there is no enough money to buy shares */
   displayNotEnoughtMoneyNotification() {
     this.noEnoughMoney = true;
     setTimeout(() => {
       this.noEnoughMoney = false;
     }, 1000);
   }
+  /**
+   * Setting new money balance using Promise to avoid 'race condition'
+   * Used with buy orders */
   setNewMoneyBalance(moneyToSpend: number, moneyInBalance: number) {
     let promise = new Promise((resolve, reject) => {
         let moneyLeft = moneyInBalance - moneyToSpend;
@@ -169,6 +189,9 @@ export class StocksInMarketComponent implements OnInit {
     });
     return promise;
   }
+  /**
+   * Setting new money balance using Promise to avoid 'race condition'
+   * Used with sell orders */
   setNewMoneyBalanceSell(moneyToReceive: number, moneyInBalance: number) {
     let promise = new Promise((resolve, reject) => {
       let newMoneyBalance = moneyInBalance + moneyToReceive;
@@ -178,6 +201,8 @@ export class StocksInMarketComponent implements OnInit {
     });
     return promise;
   }
+  /**
+   * Money transaction for buy orders */
   doMoneyTransaction(moneyToSpend: number, moneyInBalance: number) {
     if (moneyToSpend <= moneyInBalance) {
       this.setNewMoneyBalance(moneyToSpend, moneyInBalance).then(() => this.displayOrderPlacedNotification());
@@ -185,9 +210,13 @@ export class StocksInMarketComponent implements OnInit {
       this.displayNotEnoughtMoneyNotification();
     }
   }
+  /**
+   * Money transaction for sell orders */
   doMoneyTransactionSell(moneyToReceive: number, moneyInBalance: number) {
       this.setNewMoneyBalanceSell(moneyToReceive, moneyInBalance).then(() => this.displayOrderPlacedNotification());
   }
+  /**
+   * Inirialising money balance */
   initialiseMoneyBalance() {
     let promise = new Promise((resolve, reject) => {
       this.moneyBalanceService.setBalance();
